128074fa9a20ac555f371d36448bfe85e0459276
Alexis Suarez <suarezkecho@gmail.com>
2025-06-09 14:07:38 -0300

    Falto agregar el src en el from

--- src/consolas/consola_calculo_calorias_adelgazar.py
+++ src/consolas/consola_calculo_calorias_adelgazar.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from src.calculadora_indices import consumo_calorias_recomendado_para_adelgazar
-from elementos_de_programa import crear_persona, obtener_inputs
+from src.elementos_de_programa import crear_persona, obtener_inputs
 
 if __name__ == '__main__':
     peso,altura_metros,altura_centimetros,edad,genero,actividad = obtener_inputs()
