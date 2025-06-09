c4fb9c9aee67454210302cf5459b41265f265895
Alexis Suarez <suarezkecho@gmail.com>
2025-06-09 13:43:16 -0300

    Realice una modificacion en la consola_calculo_imc.py para que me detecte la ruta ddesde el script, porque me daba error al no reconocer el modulo 'src'y agregue una carpeta 'images' para colocar las capturas de las respuestas

--- src/consolas/consola_calculo_imc.py
+++ src/consolas/consola_calculo_imc.py
@@ -1,5 +1,10 @@
-from elementos_de_programa import crear_persona, obtener_inputs
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from src.elementos_de_programa import crear_persona, obtener_inputs
 from src.calculadora_indices import calcular_IMC
+
 
 if __name__ == '__main__':
     peso,altura_metros,altura_centimetros,edad,genero,actividad = obtener_inputs()
